582536002f71db63d5efaf31b53cb428c521d1b3
szwnba <szwnba@qq.com>
2024-07-23 23:42:12 +0800

    1

--- api/settings.py
+++ api/settings.py
@@ -131,13 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = 'media/'
 
